e9d9a642d305e21414acfcd2780ead6ce352758a
stivenrodriguezm <stivenrodriguez1996m@gmail.com>
2022-12-16 13:08:25 -0500

    pedidos

--- inventario/views/ordenPedidoViews.py
+++ inventario/views/ordenPedidoViews.py
@@ -36,20 +36,27 @@ def CrearOrdenPedido(request, *args, **kwargs):
 def EditarOrdenPedido(request, **kwargs):
     id_auto = kwargs['pk']
     data = request.data
+
+    venta = Ventas.objects.only("id_venta").get(id_auto = data['id_venta'])
+    proveedor = Proveedores.objects.only("id_proveedor").get(id_proveedor = data['id_proveedor'])
+    vendedor = Vendedores.objects.only("id_vendedor").get(id_vendedor = data['id_vendedor'])
+
     pedido = Pedidos.objects.get(id_auto = id_auto)
     pedido.id_orden_pedido = data['id_orden_pedido']
-    pedido.id_venta = data['id_venta']
-    pedido.id_proveedor = data['id_proveedor']
-    pedido.id_vendedor = data['id_vendedor']
+    pedido.valor_total = data['valor_total']
+    pedido.id_venta = venta
+    pedido.id_proveedor = proveedor
+    pedido.id_vendedor = vendedor
+    pedido.tela = data['tela']
     pedido.fecha = data['fecha']
     pedido.fecha_despacho = data['fecha_despacho']
-    pedido.tela = data['tela']
     pedido.enviada = data['enviada']
     pedido.recibida = data['recibida']
     pedido.save()
 
     pedido = Pedidos.objects.filter(id_auto = id_auto)
     return Response(pedido.values())
+    
 
 @api_view(['DELETE'])
 def EliminarOrdenPedido(request, **kwargs):
